994ba3576db4a617236724ab3ad88a57df9acb3b
gauravsahu <gauravsahu19@gmail.com>
2014-10-08 15:14:50 +0530

    final update

--- inquiry.py
+++ inquiry.py
@@ -22,13 +22,12 @@ class iac_recommendation(osv.osv):
 	_description = "Details about the IAC Comitte"
 	_columns = {
 		'showcause_memo': fields.many2one('showcause', 'Showcause'),
-		'gm1': fields.many2one('hr.employee', 'GM 1'),
-		'gm2': fields.many2one('hr.employee', 'GM 2'),
-		'gm3': fields.many2one('hr.employee', 'GM 3'),
+		'gm': fields.many2one('hr.employee', 'GM'),
 		'meeting_date': fields.datetime('Meeting Date'),
 		'meeting_detail': fields.html('Meeting Summary'),
-		
+		'date': fields.datetime('Date'),
 	}
+
 iac_recommendation()
 
 class cvo_recommendation(osv.osv):
@@ -37,7 +36,7 @@ class cvo_recommendation(osv.osv):
 	_columns = {
 		'cvo': fields.many2one('hr.employee','CVO'),
 		'cvo_feedback': fields.html('CVO Feedback'),
-		
+		'date': fields.datetime('Date'),
 		'cvc_needed': fields.boolean('CVC Needed'),
 	
 	
@@ -54,6 +53,7 @@ class cvc_advice(osv.osv):
 		'defence' : fields.char('Defense'),
 		'views_da' : fields.text("View and Comments of DA"),
 		'views_cvo' : fields.text('Views of CVO'),
+		'date': fields.datetime('Date'),
 	}
 cvc_advice()
 
@@ -62,8 +62,9 @@ class disciplinary_authority(osv.osv):
 	_columns = {
 		'authority_name' : fields.many2one('hr.employee','Authorize Person'),
 		'designation': fields.char('Designation'),
+		'date': fields.datetime('Date'),
 		'remark' : fields.text('Remark'),
-		'decision' : fields.selection([('minor penalty','Minor Penalty'),('major penalty','Major Penalty')],'Decision'), 
+		'decision' : fields.selection([('minorpenalty','Minor Penalty'),('majorpenalty','Major Penalty')],'Decision'), 
 	}
 disciplinary_authority()
 
@@ -89,20 +90,36 @@ class show_cause(osv.osv):
 		return True
 
 	def showcause_step5(self, cr, uid, ids, context=None):
-		self.write(cr, uid, ids, {'state' : 'step6'}, context=context)
-		return True
+		cvc_needed = self.read(cr,uid,ids,{'cvc_needed'},context=context)
+		print "Gaurav Sahu"
+		print cvc_needed[0]['cvc_needed']
+		if cvc_needed[0]['cvc_needed'] == True:
+			print "Inside IF"
+			self.write(cr, uid, ids, {'state' : 'step6'}, context=context)
+			return True
+		else:
+			print "Inside Else"
+			self.write(cr, uid, ids, {'state' : 'step7'}, context=context)
+			return True
 
 	def showcause_step6(self, cr, uid, ids, context=None):
 		self.write(cr, uid, ids, {'state' : 'step7'}, context=context)
 		return True
 
 	def showcause_step7(self, cr, uid, ids, context=None):
-		self.write(cr, uid, ids, {'state' : 'step8'}, context=context)
-		return True
-
+		decision = self.read(cr,uid,ids,{'decision'}, context=context),
+		print "Gaurav Sahu"
+		print decision[0][0]['decision']
+		if decision[0][0]['decision'] == "minorpenalty": 
+			self.write(cr, uid, ids, {'state' : 'step8'}, context=context)
+			return True
+		else:
+			self.write(cr, uid, ids, {'state' : 'step9'}, context=context)
+			return True
+		
 	_columns = {
 		'rohead_comments_line':fields.one2many('showcause.charge','showcause'),
-		'gm_list': fields.one2many('iac.recommendation' , 'showcause_memo'),
+		'gm_line': fields.one2many('iac.recommendation' , 'showcause'),
 		'title': fields.char('Title', size=128, required=True),
 		'description': fields.text('Description'),
 		'against_person': fields.many2one('hr.employee', 'Accused'),
@@ -121,12 +138,16 @@ class show_cause(osv.osv):
 			('step5', 'CVO'),
 			('step6', 'CVC'),
 			('step7','DA Penalty'),
-			('step8', 'Article Of Charge'),],"State"),
+			('step8','Ist Stage Advice'),
+			('step9', 'Article Of Charge'),],"State"),
 	}
 	_defaults = {
 		'state': 'step1'
 	}
-
+	def onchange_iac_vigilance_status(self, cr, uid, ids, vigilance_status, context=None):
+		self.write(cr, uid, ids, {'vigilance_status' : vigilance_status}, context=context)
+			
+			
 	
 show_cause()
 
@@ -201,8 +222,8 @@ class charge_sheet(osv.osv):
 		
 	def charge_step(self, cr, uid, context=None):
 		sheet_id = 14
-		charge_sheet_obj = self.pool.get('charge.sheet').browse(cr, uid, charge_sheet_id)
-		create_date = charge_sheet_obj.start_date
+		charge_sheet_obj = self.pool.get('charge.sheet').browse(cr, uid, sheet_id)
+		create_date = charge_sheet_obj.create_date
 		self.write(cr, uid, [14], {'state' : 'step4'}, context=context)
 		return True
 
@@ -228,13 +249,14 @@ class charge_sheet(osv.osv):
 
 	_columns = {
 		"showcause_id" : fields.many2one('showcause',"Showcause"),
+		'title' : fields:.char('Title'),
 		"witness_name" : fields.many2one('hr.employee',"Witness Name"),
 		"first_advice" : fields.text('First Stage Advice',readonly=True),
 		'vigilance_status': fields.char('Vigilance Status',readonly=True),
 		'inquiry' : fields.boolean('Inquiry Status'),
 		'start_date' : fields.datetime('Date'),
 		'da_comments' : fields.text("Da Comments"),
-		'cso_comments' : fields.text("CSO Feedback"),
+		'cso_comments' : fields.text("CSO Submission"),
 		'da_penality_text' : fields.text('DA Penalty Text'),
 		'concure_ia' : fields.boolean('Concure with IA'),
 		'cvo_feedback' : fields.text("CVO Feedback"),
